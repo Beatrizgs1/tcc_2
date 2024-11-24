a39018312f4218de6e6612c542abd53148c3b8c5
Beatriz Gonçalves de Souza <goncalvesbeatriz633@gmail.com>
2024-11-24 22:55:45 +0000

    ajustes na home

--- src/pages/Quemsomos.jsx
+++ src/pages/Quemsomos.jsx
@@ -9,7 +9,7 @@ export function Quemsomos() {
       <Nav />
       <div className=" justify-center mx-20 mt-5 mb-[160px]">
         <h1 className="text-2xl md:text-4xl font-semibold text-purple  md:mb-[40px] mb-12 mt-16 font-titulo text-center  ml-[20px]">Desenvolvedores</h1>
-        <div className="max-w-7xl mx-auto ">
+        <div className="max-w-7xl mx-auto mt-[90px] ">
           <Carossel />
         </div>
       </div>
