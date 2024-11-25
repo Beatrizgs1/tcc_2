08fe7e5757955d5f87f705cd995d49c8346d2c0a
Beatriz Gonçalves de Souza <goncalvesbeatriz633@gmail.com>
2024-11-25 16:31:25 +0000

    tt

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,7 +5,18 @@ import { Link } from 'react-router-dom'
 import logoSite from '../assets/logoSite.svg'
 import { GoogleLogin } from '@react-oauth/google';
 
-
+module.exports = {
+    darkMode: 'class', // Permite alternar entre modos claro e escuro
+    theme: {
+      extend: {
+        colors: {
+          // Suas cores personalizadas
+        },
+      },
+    },
+    plugins: [],
+  }
+  
 
 
 
